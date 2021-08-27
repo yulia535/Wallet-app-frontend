99c8ca3401572868d7592d89fc8fe2281a11fc89
Kateryna Gordiienko <gordkat@gmail.com>
2021-08-27 22:58:16 +0200

    add info into statistic table

--- src/components/Table/TableItem/TableItem.js
+++ src/components/Table/TableItem/TableItem.js
@@ -1,13 +1,13 @@
 import styles from './TableItem.module.css';
-const TableItem = ({ color, type, quantity }) => (
+const TableItem = ({ color, amount, category }) => (
   <li className={styles.table_item}>
     <div
       className={styles.marker_color}
       style={{ backgroundColor: color }}
     ></div>
     <p className={styles.description_consumption}>
-      <span className={styles.name_consumption}>{type}</span>
-      <span className={styles.quantity_consumption}>{quantity}</span>
+      <span className={styles.name_consumption}>{category}</span>
+      <span className={styles.quantity_consumption}>{amount}</span>
     </p>
   </li>
 );
