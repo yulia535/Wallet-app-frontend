f3da68252786c3a91e0bd4e09fed4161c34270ec
Yulia Sukhar <yulia-s@i.ua>
2021-08-30 13:51:44 +0300

    hoverButton, nameApp

--- src/views/LoginView/LoginView.js
+++ src/views/LoginView/LoginView.js
@@ -1,49 +1,13 @@
 import LoginForm from '../../components/LoginForm'
 import styles from '../LoginView/loginView.module.css'
-// import React, { Component } from 'react';
+
 
 const LoginView = () => (
   <div className={styles.viewContainer}>
-     {/* {window.innerWidth > 767 ? (
-       <>
-      <span>Finance App</span>
-      <LoginForm/>
-      </>
-  ) : (
-      <LoginForm/>
-    )} */}
+    <h2 className={styles.nameApp}>Finance App</h2>
     <LoginForm/>
   </div>
 );
 
-// class LoginView extends Component {
-// state = { width: window.innerWidth };
-//   componentDidMount() {
-//     window.addEventListener("resize", setWidth);
-//     this.resize();
-// }
-// const setWidth = (event)=>{
-//   this.setState({width: window.innerWidth})
-// }
-// resize() {
-//     this.setState({width: window.innerWidth});
-// }
- 
 
-//   render() {
-//     const width = this.state;
-    
-//     return  <div className={styles.viewContainer}>
-//      {width > 767 ? (
-//        <>
-//       <span>Finance App</span>
-//       <LoginForm/>
-//       </>
-//   ) : (
-//       <LoginForm/>
-//     )}
-//     {/* <LoginForm/> */}
-//   </div>
-// }
-// }
 export default LoginView;
