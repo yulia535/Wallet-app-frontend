47f525cbc5838cefad52d4e4f317b2ffc9961f1e
Kateryna Gordiienko <gordkat@gmail.com>
2021-08-31 15:30:22 +0200

    balance

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,14 +1,19 @@
 import { useSelector } from 'react-redux';
 import { transactionsSelectors } from '../../redux/transactions';
 import s from './Balance.module.css';
+import formatCurrency from '../../utils/formatCurrency';
 
 const Ballance = () => {
-  const total = useSelector(transactionsSelectors.getBalance);
+  let balance = useSelector(transactionsSelectors.getBalance);
+  if (balance) {
+    balance = formatCurrency(balance);
+  }
+
   return (
     <div className={s.ballanceContainer}>
       <h2 className={s.ballanceTitle}>Ваш баланс</h2>
       <p className={s.ballance}>
-        <span className={s.hryvnia}>&#8372;{total}</span>
+        <span className={s.hryvnia}>&#8372; {balance || '0.00'}</span>
       </p>
     </div>
   );
--- src/redux/transactions/transactions-selectors.js
+++ src/redux/transactions/transactions-selectors.js
@@ -1,27 +1,14 @@
-// import { createSelector } from '@reduxjs/toolkit';
-
 const getAllTransactions = state => state.transactions.items;
 const getTransactionsByDate = state => state.transactions.itemsByDate;
 const getMonth = state => state.transactions.month;
 const getYear = state => state.transactions.year;
-const getLoading = state => state.transactions.loading;
-
 const getBalance = state => state.transactions.balance;
-
-// const getVisibleTransactions = createSelector(
-//   [getAllTransactions, getMonth, getYear],
-//   (items, month, year) => {
-//     console.log(items, month, year);
-//   },
-// );
 
 const transactionsSelectors = {
   getAllTransactions,
   getTransactionsByDate,
   getMonth,
   getYear,
-  getLoading,
-  // getVisibleTransactions,
   getBalance,
 };
 export default transactionsSelectors;
