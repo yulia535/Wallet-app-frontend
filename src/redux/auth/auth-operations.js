d2d44fbffec76188edd25490ba73cd545a91f9c8
Anna Shevchenko <uaskoa@gmail.com>
2021-09-01 01:50:13 +0300

    heroku

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import authActions from './auth-actions';
 
-axios.defaults.baseURL = 'http://localhost:3001';
+// axios.defaults.baseURL = 'http://localhost:3001';
 
-// axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'https://wallet-app-backend.herokuapp.com/';
+
 
 const token = {
   set(token) {
